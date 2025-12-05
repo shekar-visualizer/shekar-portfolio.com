e25d4e8f4ed763db3676395b2b8446cffb89af30
shekar-visualizer <shekar.nagulapalli@gmail.com>
2025-12-05 18:59:22 +0530

    Add 1 new portfolio file

--- assestsName.js
+++ assestsName.js
@@ -1,4 +1,6 @@
 const PhotoshopFiles = [
+  { src: "IMG_0036.jpeg", title: "IMG_0036" },
+  { src: "PNG image 2.png", title: "PNG image 2" },
   { src: "img1.jpg", title: "Img1" },
   { src: "sep23.jpg", title: "Take a look!" },
   { src: "sep25.jpg", title: "Take a look!" },
